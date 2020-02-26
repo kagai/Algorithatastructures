56af49636230f554c39608a60dd46c21d77d051f
kagai <shaddy.mwangi@gmail.com>
2020-02-26 22:04:47 +0300

    adding addTohead function

---
+++ datastructures.js
@@ -0,0 +1,16 @@
+// linked list 
+
+function LinkedList(){
+    this.head = null;
+    this.tail = null;
+};
+
+function Node(value,next,prev){
+    this.value = value ;
+    this.next = next ;
+    this.prev = prev ;
+};
+
+LinkedList.prototype.addToHead = function(value){
+
+}
