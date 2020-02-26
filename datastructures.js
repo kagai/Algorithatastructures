132fdc1ebf2ad6644b65ed8e8842c5d9983830f5
kagai <shaddy.mwangi@gmail.com>
2020-02-26 22:11:20 +0300

    adding add to tail method

--- datastructures.js
+++ datastructures.js
@@ -12,5 +12,15 @@ function Node(value,next,prev){
 };
 
 LinkedList.prototype.addToHead = function(value){
-
+  let newNode = new Node(value,this.head,null);
+  if(this.head)this.head.prev = newNode;
+  else this.tail = newNode;
+  this.head = newNode;
 }
+
+linkedList.prototype.addToTail = function(value){
+   let newNode = new Node(value,null,this.tail);
+   if(this.tail) this.tail.next = newNode;
+   else this.head = newNode;
+   this.tail = newNode;
+};
