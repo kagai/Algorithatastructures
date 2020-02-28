86be236e307f3a06461632dbce9d71a20f4e3350
kagai <shaddy.mwangi@gmail.com>
2020-02-28 11:50:26 +0300

    memonized fibonnaci

--- Algo.js
+++ Algo.js
@@ -278,4 +278,28 @@ function binarySearch(numArray,key){
  else return false;
 }
 
-// -------------------------------------------------
+// --------------------fibonacci-----------------------------
+// very bad time complextity 
+// exponential time complexity
+function fibonacci(position){
+  //base case 
+  // use recursion
+  if(position<3) return 1;
+  else return fibonacci(position-1) + fibonacci(position -2);
+};
+
+
+// -----------------memonized fibonacci----------------------
+// better than the first one 
+function fibMemo(index,cache){
+ cache = cache || [];
+ 
+ if(cache[index]) return cache[index];
+ else{
+   if(index < 3) return ;
+   else{
+     cache[index] = fibMemo(index-1,cache) + fibMemo(index-2,cache);
+   }
+ }
+ return cache[index];
+};
